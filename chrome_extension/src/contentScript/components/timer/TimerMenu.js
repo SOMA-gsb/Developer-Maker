cf06cfdd8750e9326b51e468c2b503fc6de9b1a3
CHO EUN JUNG <0516dmswjd@gmail.com>
2023-07-19 11:01:26 +0900

    feat reflect timer setting

--- chrome_extension/src/contentScript/components/timer/TimerMenu.js
+++ chrome_extension/src/contentScript/components/timer/TimerMenu.js
@@ -11,12 +11,32 @@ function TimerMenu() {
     const probNum = window.location.href.split('/').reverse()[0];
     const timerGroup = document.getElementsByClassName('timer-group');
 
-    chrome.storage.local.get([`${probNum}Timer`, `${probNum}isRunning`], (res) => {
+    chrome.storage.local.get([`${probNum}Timer`, `${probNum}isRunning`, 'timerSet'], (res) => {
         if (`${probNum}Timer` in res && !(res[`${probNum}Timer`] == false && res[`${probNum}isRunning`] == false)) {
             for (let i = 0; i < timerGroup.length; ++i) {
                 timerGroup[i].style.display = 'block';
                 console.log("block");
             }
+        }
+        else if (res.timerSet === 'autoTimer') {
+            for (let i = 0; i < timerGroup.length; ++i) {
+                timerGroup[i].style.display = 'block';
+                console.log("block");
+            }
+
+            chrome.storage.local.set({
+                [`${probNum}Timer`]: true,
+                [`${probNum}TimeOption`]: 3600,
+                [`${probNum}Start`]: Date.now(),
+                [`${probNum}isRunning`]: true,
+                [`${probNum}Stop`]: Date.now(),
+            })
+
+            const restTime = document.getElementById("rest-time-text");
+            restTime.innerHTML = `남은&nbsp시간&nbsp&nbsp&nbsp<b>01:00:00</b>`;
+
+            const timerStopTxt = document.getElementById('timer-stop-text');
+            timerStopTxt.innerHTML = '일시정지';
         }
         else {
             for (let i = 0; i < timerGroup.length; ++i) {
--- chrome_extension/src/popup/components/TimerSet.js
+++ chrome_extension/src/popup/components/TimerSet.js
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
+
 function TimerSet() {
+    const [timerSet, setTimerSet] = useState('setTimer');
+
+    useEffect(() => {
+        chrome.storage.local.get(['timerSet'], (res) => {
+            if (res.timerSet == 'autoTimer') setTimerSet('autoTimer');
+            console.log("use " + timerSet);
+            
+            document.getElementById(timerSet).defaultChecked = true;
+        })
+    })
+
+    function updateTimerSet(e) {
+        setTimerSet(e.target.value);
+        chrome.storage.local.set({
+            timerSet: e.target.value
+        })
+    }
+
     return (
         <div>
             <h2 id="timer-title">Timer Setting</h2>
             <div className="timer-menu">
-                <input type="radio" id="setTimer" name="timer-set" value="setTimer" className='timerSet' checked />
+                <input type="radio" id="setTimer" name="timer-set" value="setTimer" className='timerSet' onChange={updateTimerSet} />
                 <label htmlFor="setTimer" className='timerSet'>Timer starts when you press the button</label>
             </div>
             <div className="timer-menu">
-                <input type="radio" id="autoTimer" name="timer-set" value="autoTimer" className='timerSet' />
+                <input type="radio" id="autoTimer" name="timer-set" value="autoTimer" className='timerSet' onChange={updateTimerSet} />
                 <label htmlFor="autoTimer" className='timerSet'>Timer starts automatically when you enter the problem page</label>
             </div>
         </div>
