8b009fdb1e2f55ae4924c8785b8f3c42b4cf8374
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2023-10-06 20:10:44 +0900

    Login page and SSO web design

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Main, Register, Error, TagList } from "./pages";
+import { Main, Register, Login, Error, TagList } from "./pages";
 
 function App() {
 	return (
@@ -8,6 +8,7 @@ function App() {
 			<Routes className="page">
 				<Route path="/" element={<Main />} />
 				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/taglist" element={<TagList />} />
 				<Route path="*" element={<Error errorno={404} />} />
 			</Routes>
--- web/src/pages/Register.js
+++ web/src/pages/Register.js
@@ -1,63 +1,62 @@
 import "./Register.css";
+import "./Login-and-Register.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Register() {
 	return (
 		<div className="register-page">
-			<div className="register-information">
+			<div className="DM-information">
 				<div style={{ height: "10%" }} />
-				<div id="register-information-title">Developer Maker</div>
-				<div id="register-information-subtitle">
-					Improve Your Algorithm Skill
-				</div>
+				<div id="DM-information-title">Developer Maker</div>
+				<div id="DM-information-subtitle">Improve Your Algorithm Skill</div>
 				<div style={{ height: "50%" }} />
 			</div>
-			<div className="register-form">
-				<div id="register-form-title">Register</div>
-				<div className="register-form-input">
+			<div className="DM-form">
+				<div id="DM-form-title">Register</div>
+				<div className="DM-form-input">
 					<input
-						className="register-form-input-component"
+						className="DM-form-input-component"
 						type="text"
 						placeholder="Username"
 						name="username"
 						required
 					/>
 					<input
-						className="register-form-input-component"
+						className="DM-form-input-component"
 						type="text"
 						placeholder="Email"
 						name="email"
 						required
 					/>
 					<input
-						className="register-form-input-component"
+						className="DM-form-input-component"
 						type="password"
 						placeholder="Password"
 						name="password"
 						required
 					/>
 					<input
-						className="register-form-input-component"
+						className="DM-form-input-component"
 						type="password"
 						placeholder="Confirm Password"
 						name="confirm-password"
 						required
 					/>
 					<input
-						className="register-form-input-component"
+						className="DM-form-input-component"
 						type="text"
 						placeholder="BOJ ID"
 						name="boj-id"
 					/>
 					<input
-						className="register-form-input-component"
+						className="DM-form-input-component"
 						type="text"
 						placeholder="Codeforces ID"
 						name="codeforces-id"
 					/>
 					<input
-						className="register-form-input-component"
+						className="DM-form-input-component"
 						type="text"
 						placeholder="Atcoder ID"
 						name="atcoder-id"
@@ -77,8 +76,8 @@ function Register() {
 						에 동의합니다.
 					</div>
 				</div>
-				<div className="register-form-button">
-					<div className="register-form-button-components" type="submit">
+				<div className="DM-form-button">
+					<div className="DM-form-button-components" type="submit">
 						Sign Up
 					</div>
 				</div>
